90f98809df4e3fd578e065db01d7627e3f8ea690
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-08 11:57:42 +0200

    Add dotenv

--- db/database.py
+++ db/database.py
@@ -2,9 +2,12 @@
 
 import os
 
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
+load_dotenv()
 
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 if SQLALCHEMY_DATABASE_URL.startswith("postgresql://"):
