e3e49a62e103727a5c655711ac04cb33e66c2746
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-12 11:43:37 +0200

    Add tests

--- app/tests/test_dishes.py
+++ app/tests/test_dishes.py
@@ -1,25 +1,31 @@
 """Tests for dishes.py"""
 
 
+def test_create_dish(test_app):
+    """Create a dish."""
+    data = {
+        "id": 1,
+        "name": "ciwi core",
+        "description": "Creativity is intelligence having fun.",
+        "image_url": "https://video.szekwanyuen1.net/HouseholdKitchenAppliances",
+        "cost": 750.09,
+    }
+    response = test_app.post("/dishes", json=data)
+    assert response.status_code == 201
+    assert response.json()["id"] == 1
+    assert response.json()["name"] == "ciwi core"
+
+
 def test_get_dish(test_app):
-    """Test get a dish by id, 404, all dishes."""
+    """Test get a dish by id, 404"""
     response = test_app.get("/dishes/1")
     assert response.status_code == 200
 
     response = test_app.get("/dishes/100")
     assert response.status_code == 404
 
+
+def test_get_all_dishes(test_app):
+    """Test get all dishes."""
     response = test_app.get("/dishes")
     assert response.status_code == 200
-
-
-def test_create_dish(test_app):
-    """Create a dish."""
-    data = {
-      "name": "ciwi core",
-      "description": "Creativity is intelligence having fun.",
-      "image_url": "https://video.szekwanyuen1.net/HouseholdKitchenAppliances",
-      "cost": 750.09
-    }
-    response = test_app.post("/dishes", json=data)
-    assert response.status_code == 201
