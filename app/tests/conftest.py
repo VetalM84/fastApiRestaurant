4d0c677e59c75f22d17a5156394ae64306e50c1b
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-12 11:41:01 +0200

    Fixes to clean up the DB after each test

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,7 +1,7 @@
 """Test client config file."""
 
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
 from sqlalchemy.orm import sessionmaker
 from starlette.testclient import TestClient
 
@@ -15,10 +15,53 @@ engine = create_engine(
 )
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 
 
+# These two event listeners are only needed for sqlite for proper
+# SAVEPOINT / nested transaction support. Other databases like postgres don't need them.
+@event.listens_for(engine, "connect")
+def do_connect(dbapi_connection, connection_record):
+    """Disable pysqlite's emitting of the BEGIN statement entirely.
+    also stops it from emitting COMMIT before any DDL."""
+    dbapi_connection.isolation_level = None
+
+
+@event.listens_for(engine, "begin")
+def do_begin(conn):
+    """Emit our own BEGIN."""
+    conn.exec_driver_sql("BEGIN")
+
+
+@pytest.fixture()
+def session():
+    """This fixture is the main difference to before. It creates a nested
+    transaction, recreates it when the application code calls session.commit
+    and rolls it back at the end."""
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+
+    nested = connection.begin_nested()
+
+    @event.listens_for(session, "after_transaction_end")
+    def end_savepoint(session, transaction):
+        """If the application code calls session.commit, it will end the nested
+        transaction. Need to start a new one when that happens."""
+        nonlocal nested
+        if not nested.is_active:
+            nested = connection.begin_nested()
+
+    yield session
+
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
 def override_get_db():
+    """Redirect request to use testing DB."""
     try:
         db = TestingSessionLocal()
         yield db
