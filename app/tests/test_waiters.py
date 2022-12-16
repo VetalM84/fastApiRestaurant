f46238413257eeb357ad46d63a21a3bea986e49a
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-16 12:21:27 +0200

    Add test

--- app/tests/test_waiters.py
+++ app/tests/test_waiters.py
@@ -11,3 +11,14 @@ def test_get_waiter(test_app):
     """Test Get a waiter by id."""
     response = test_app.get("/waiters/1")
     assert response.status_code == 200
+
+
+def test_create_waiter(test_app):
+    """Test create a waiter."""
+    data = {
+        "username": "username",
+        "password": "11111111",
+    }
+    response = test_app.post("/waiters", json=data)
+    assert response.status_code == 201
+
