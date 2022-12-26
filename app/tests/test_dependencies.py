e194b2bedaeaf5c1bf40e16aa52fb7bc55fdc1f8
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-26 18:23:46 +0200

    Wrap tests to class

--- app/tests/test_dependencies.py
+++ app/tests/test_dependencies.py
@@ -8,51 +8,52 @@ from fastapi import HTTPException
 from app.crud.dependencies import JWTBearer, get_current_user
 
 
-def test_get_current_user(db_session, mocker):
-    """Test get current user success."""
-    mocker.patch("app.crud.utils.get_user_by_username", return_value="string")
-    result = asyncio.run(
-        get_current_user(
-            db=db_session,
-            token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJpYXQiOjE1MTYyMzkwMjJ9.gKlStbKJNJyTrI-halDo1aRbgMbFaUVoLfb0-HM_bUo",
-        )
-    )
-    assert result == "string"
+class TestDependencies:
+    """Class contains tests for dependencies.py"""
 
-
-def test_get_current_user_fails(db_session):
-    """Test get current user fails with user is None and bad token."""
-    # user is None
-    with pytest.raises(HTTPException):
-        asyncio.run(
+    def test_get_current_user(self, db_session, mocker):
+        """Test get current user success."""
+        mocker.patch("app.crud.utils.get_user_by_username", return_value="string")
+        result = asyncio.run(
             get_current_user(
                 db=db_session,
-                token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NzEzNTU0MTF9.NbAAkmEerO_Wm8lNQeLfd_XVIGn-k8-gekVrcgpHUfA",
+                token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJpYXQiOjE1MTYyMzkwMjJ9.gKlStbKJNJyTrI-halDo1aRbgMbFaUVoLfb0-HM_bUo",
             )
         )
-    # bad token
-    with pytest.raises(HTTPException):
-        asyncio.run(
-            get_current_user(
-                db=db_session,
-                token="JleHAiOjE2NzEzNTU0MTF9.NbAAkmEerO_Wm8lNQeLfd_XVIGn-k8-gekVrcgpHUfA",
+        assert result == "string"
+
+    def test_get_current_user_fails(self, db_session):
+        """Test get current user fails with user is None and bad token."""
+        # user is None
+        with pytest.raises(HTTPException):
+            asyncio.run(
+                get_current_user(
+                    db=db_session,
+                    token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NzEzNTU0MTF9.NbAAkmEerO_Wm8lNQeLfd_XVIGn-k8-gekVrcgpHUfA",
+                )
             )
+        # bad token
+        with pytest.raises(HTTPException):
+            asyncio.run(
+                get_current_user(
+                    db=db_session,
+                    token="JleHAiOjE2NzEzNTU0MTF9.NbAAkmEerO_Wm8lNQeLfd_XVIGn-k8-gekVrcgpHUfA",
+                )
+            )
+
+    def test_jwt_bearer(self):
+        """Test verify_token method in JWTBearer class."""
+        jwt_bearer = JWTBearer()
+        # valid token
+        result = jwt_bearer.verify_token(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJpYXQiOjE1MTYyMzkwMjJ9.gKlStbKJNJyTrI-halDo1aRbgMbFaUVoLfb0-HM_bUo"
         )
-
-
-def test_jwt_bearer():
-    """Test verify_token method in JWTBearer class."""
-    jwt_bearer = JWTBearer()
-    # valid token
-    result = jwt_bearer.verify_token(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJpYXQiOjE1MTYyMzkwMjJ9.gKlStbKJNJyTrI-halDo1aRbgMbFaUVoLfb0-HM_bUo"
-    )
-    assert result
-    # wrong token
-    result = jwt_bearer.verify_token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ")
-    assert not result
-    # expired token
-    result = jwt_bearer.verify_token(
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NzE1MzUxNDN9.iSMmHiQhBDrZtkhR07cSuTlLTT5cYFX5jXwgrLfqYIU"
-    )
-    assert not result
+        assert result
+        # wrong token
+        result = jwt_bearer.verify_token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ")
+        assert not result
+        # expired token
+        result = jwt_bearer.verify_token(
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJzdHJpbmciLCJleHAiOjE2NzE1MzUxNDN9.iSMmHiQhBDrZtkhR07cSuTlLTT5cYFX5jXwgrLfqYIU"
+        )
+        assert not result
--- app/tests/test_utils.py
+++ app/tests/test_utils.py
@@ -13,67 +13,70 @@ from app.crud.utils import (
 from app.db.models import Waiter
 
 
-@pytest.mark.parametrize("tip_included,expected_output", [(False, 0.0), (True, 5.0)])
-def test_tip(mocker, tip_included, expected_output):
-    """Test tip amount."""
-    mock_bill = mocker.Mock(tip_percent=5, tip_included=tip_included)
-    result = tip(amount=100, bill=mock_bill)
-    assert result == expected_output
+class TestUtils:
+    """Class contains tests for utils.py"""
 
-
-def test_verify_password(mocker):
-    """Test verify password."""
-    mocker.patch("app.crud.utils.pwd_context.verify", return_value=True)
-    result = verify_password(
-        plain_password="1111",
-        hashed_password="$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW",
+    @pytest.mark.parametrize(
+        "tip_included,expected_output", [(False, 0.0), (True, 5.0)]
     )
-    assert result
+    def test_tip(self, mocker, tip_included, expected_output):
+        """Test tip amount."""
+        mock_bill = mocker.Mock(tip_percent=5, tip_included=tip_included)
+        result = tip(amount=100, bill=mock_bill)
+        assert result == expected_output
 
+    def test_verify_password(self, mocker):
+        """Test verify password."""
+        mocker.patch("app.crud.utils.pwd_context.verify", return_value=True)
+        result = verify_password(
+            plain_password="1111",
+            hashed_password="$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW",
+        )
+        assert result
 
-def test_get_password_hash(mocker):
-    """Test get password hash."""
-    mocker.patch(
-        "app.crud.utils.pwd_context.hash",
-        return_value="$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW",
+    def test_get_password_hash(self, mocker):
+        """Test get password hash."""
+        mocker.patch(
+            "app.crud.utils.pwd_context.hash",
+            return_value="$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW",
+        )
+        result = get_password_hash(password="1111")
+        assert result == "$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW"
+
+    def test_authenticate_user(self, db_session, mocker):
+        """Test authenticate user."""
+        waiter = Waiter(
+            id=10,
+            username="string",
+            password="$12$BQhTQ6/OLAmkG/LU6G2J2.ngFk6EI9hBjFNjeTnpj2eVfQ3DCAtT.",
+        )
+        mocker.patch(
+            "app.crud.utils.get_user_by_username",
+            return_value=waiter,
+        )
+        mocker.patch(
+            "app.crud.utils.verify_password",
+            return_value=True,
+        )
+        result = authenticate_user(username="string", password="1111", db=db_session)
+        assert result == waiter
+
+        # verify_password fails
+        mocker.patch(
+            "app.crud.utils.verify_password",
+            return_value=False,
+        )
+        result = authenticate_user(username="string", password="1111", db=db_session)
+        assert not result
+
+    @pytest.mark.parametrize(
+        "expires_delta,expected_output",
+        [(timedelta(minutes=30), "encoded"), (None, "encoded")],
     )
-    result = get_password_hash(password="1111")
-    assert result == "$2b$12$OvWlVVOnafTbNMwiOkDqSOQHkfCy5vj2xgQdZOz6QHZx6ul6VSVtW"
-
-
-def test_authenticate_user(db_session, mocker):
-    """Test authenticate user."""
-    waiter = Waiter(
-        id=10,
-        username="string",
-        password="$12$BQhTQ6/OLAmkG/LU6G2J2.ngFk6EI9hBjFNjeTnpj2eVfQ3DCAtT.",
-    )
-    mocker.patch(
-        "app.crud.utils.get_user_by_username",
-        return_value=waiter,
-    )
-    mocker.patch(
-        "app.crud.utils.verify_password",
-        return_value=True,
-    )
-    result = authenticate_user(username="string", password="1111", db=db_session)
-    assert result == waiter
-
-    # verify_password fails
-    mocker.patch(
-        "app.crud.utils.verify_password",
-        return_value=False,
-    )
-    result = authenticate_user(username="string", password="1111", db=db_session)
-    assert not result
-
-
-@pytest.mark.parametrize(
-    "expires_delta,expected_output",
-    [(timedelta(minutes=30), "encoded"), (None, "encoded")],
-)
-def test_create_access_token(mocker, expires_delta, expected_output):
-    """Test create access token."""
-    mocker.patch("jose.jwt.encode", return_value="encoded")
-    result = create_access_token(data={"sub": "string"}, expires_delta=expires_delta)
-    assert result == expected_output
+    def test_create_access_token(self, mocker, expires_delta, expected_output):
+        """Test create access token."""
+        mocker.patch("jose.jwt.encode", return_value="encoded")
+        result = create_access_token(
+            data={"sub": "string"}, expires_delta=expires_delta
+        )
+        assert result == expected_output
