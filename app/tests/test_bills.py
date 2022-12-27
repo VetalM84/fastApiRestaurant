ac6371f402332f3bbc23807f2bb4170a900f4f1d
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-27 22:43:25 +0200

    Wrap tests to class

--- app/tests/test_bills.py
+++ app/tests/test_bills.py
@@ -1,7 +1,10 @@
 """Tests for bills.py"""
 
 
-def test_get_bill(test_client):
-    """Test get a bill by id with a list of the ordered dishes."""
-    response = test_client.get("/bills/4")
-    assert response.status_code == 200
+class TestBill:
+    """Class contains tests for bills.py."""
+
+    def test_get_bill(self, test_client):
+        """Test get a bill by id with a list of the ordered dishes."""
+        response = test_client.get("/bills/4")
+        assert response.status_code == 200
--- app/tests/test_dishes.py
+++ app/tests/test_dishes.py
@@ -1,31 +1,32 @@
 """Tests for dishes.py"""
 
 
-def test_create_dish(test_client):
-    """Create a dish."""
-    data = {
-        "id": 1,
-        "name": "ciwi core",
-        "description": "Creativity is intelligence having fun.",
-        "image_url": "https://video.szekwanyuen1.net/HouseholdKitchenAppliances",
-        "cost": 750.09,
-    }
-    response = test_client.post("/dishes", json=data)
-    assert response.status_code == 201
-    assert response.json()["id"] == 1
-    assert response.json()["name"] == "ciwi core"
+class TestDish:
+    """Class contains tests for dishes.py."""
 
+    def test_create_dish(self, test_client):
+        """Create a dish."""
+        data = {
+            "id": 1,
+            "name": "ciwi core",
+            "description": "Creativity is intelligence having fun.",
+            "image_url": "https://video.szekwanyuen1.net/HouseholdKitchenAppliances",
+            "cost": 750.09,
+        }
+        response = test_client.post("/dishes", json=data)
+        assert response.status_code == 201
+        assert response.json()["id"] == 1
+        assert response.json()["name"] == "ciwi core"
 
-def test_get_dish(test_client):
-    """Test get a dish by id, 404"""
-    response = test_client.get("/dishes/1")
-    assert response.status_code == 200
+    def test_get_dish(self, test_client):
+        """Test get a dish by id, 404"""
+        response = test_client.get("/dishes/1")
+        assert response.status_code == 200
 
-    response = test_client.get("/dishes/100")
-    assert response.status_code == 404
+        response = test_client.get("/dishes/100")
+        assert response.status_code == 404
 
-
-def test_get_all_dishes(test_client):
-    """Test get all dishes."""
-    response = test_client.get("/dishes")
-    assert response.status_code == 200
+    def test_get_all_dishes(self, test_client):
+        """Test get all dishes."""
+        response = test_client.get("/dishes")
+        assert response.status_code == 200
