42a24307259cb968d9a379f5f02614d525f3c270
Vitalii Mytenko <vetalmit@gmail.com>
2022-12-28 23:08:02 +0200

    Add tests

--- app/tests/test_waiters.py
+++ app/tests/test_waiters.py
@@ -1,24 +1,33 @@
 """Tests for waiters.py"""
 
-
-def test_get_all_waiters(test_app):
-    """Test get all waiters."""
-    response = test_app.get("/waiters")
-    assert response.status_code == 200
+import pytest
 
 
-def test_get_waiter(test_app):
-    """Test Get a waiter by id."""
-    response = test_app.get("/waiters/1")
-    assert response.status_code == 200
+@pytest.mark.usefixtures("db_data")
+class TestWaiter:
+    """Class contains tests for waiters.py."""
 
+    def test_create_waiter(self, test_client):
+        """Test create a waiter."""
+        data = {
+            "username": "username",
+            "password": "11111111",
+        }
+        response = test_client.post("/waiters", json=data)
+        assert response.status_code == 201
+        assert response.json()["username"] == data["username"]
 
-def test_create_waiter(test_app):
-    """Test create a waiter."""
-    data = {
-        "username": "username",
-        "password": "11111111",
-    }
-    response = test_app.post("/waiters", json=data)
-    assert response.status_code == 201
+    def test_get_waiter(self, test_client, access_token):
+        """Test Get a waiter by id."""
+        test_client.headers = {
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {access_token}",
+        }
+        response = test_client.get("/waiters/1")
+        assert response.status_code == 200
 
+    def test_get_all_waiters(self, test_client):
+        """Test get all waiters."""
+        response = test_client.get("/waiters")
+        assert response.status_code == 200
+        assert len(response.json()) == 2
